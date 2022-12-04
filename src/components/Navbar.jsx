3103cd60c69e95dbd9d0d1599824566bafef6d9e
Diego Soto <juandisoca@gmail.com>
2022-12-03 23:48:31 -0600

    navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,34 +11,32 @@ const Navbar = () => {
       </div>
 
       {/* Menu */}
-      <div>
-        <ul className='flex'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Experience</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-
-      {/* Hamburger */}
-      <div className='hidden'>
-        <FaBars />
-      </div>
-
-      {/* Mobile menu */}
-      <ul className='hidden'>
+      <ul className='hidden md:flex '>
         <li>Home</li>
         <li>About</li>
         <li>Experience</li>
         <li>Skills</li>
         <li>Work</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* Hamburger */}
+      <div className='md:hidden z-10'>
+        <FaBars />
+      </div>
+
+      {/* Mobile menu */}
+      <ul className='hidden absolute top-0 left-0 w-full h-screen  bg-[#0a192f] flex flex-col justify-center items-center'>
+        <li className='py-6 text-4xl'  >Home</li>
+        <li className='py-6 text-4xl'  >About</li>
+        <li className='py-6 text-4xl'  >Experience</li>
+        <li className='py-6 text-4xl'  >Skills</li>
+        <li className='py-6 text-4xl'  >Work</li>
       </ul>
 
 
       {/* socials icons */}
-      <div></div>
+      <div className='hidden'></div>
 
     </div>
   );
