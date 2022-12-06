7143728c670be215322c769a6f13e5702643d25b
Diego Soto <juandisoca@gmail.com>
2022-12-06 14:26:15 -0600

    Add mobile hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/logo.png'
 import { FaBars, FaTimes } from 'react-icons/fa'
 
 const Navbar = () => {
+
+  const [nav, setNave] = useState(false)
+
+  const handleClick = () => {
+    setNave(!nav)
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
 
@@ -21,12 +41,12 @@ const Navbar = () => {
       </ul>
 
       {/* Hamburger */}
-      <div className='md:hidden z-10'>
-        <FaBars />
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
       {/* Mobile menu */}
-      <ul className='hidden absolute top-0 left-0 w-full h-screen  bg-[#0a192f] flex flex-col justify-center items-center'>
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen  bg-[#0a192f] flex flex-col justify-center items-center'}>
         <li className='py-6 text-4xl'  >Home</li>
         <li className='py-6 text-4xl'  >About</li>
         <li className='py-6 text-4xl'  >Experience</li>
