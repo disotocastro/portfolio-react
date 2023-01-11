7027116201f58c32bfbe01c65a902abddbf0128f
Diego Soto <juandisoca@gmail.com>
2023-01-10 23:16:09 -0600

    ADD React smooth scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,7 @@ import {
 import logo from '../assets/logo.png'
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
 
@@ -27,12 +28,32 @@ const Navbar = () => {
 
       {/* Menu */}
       <ul className='hidden md:flex '>
-        <li>Home</li>
-        <li>About</li>
-        <li>Experience</li>
-        <li>Skills</li>
-        <li>Work</li>
-        <li>Contact</li>
+        <li>
+          <Link to="home" smooth={true} duration={500} >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={500} >
+            About
+          </Link>
+        </li>
+
+        <li>
+          <Link to="skills" smooth={true} duration={500} >
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to="work" smooth={true} duration={500} >
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} duration={500} >
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/* Hamburger */}
